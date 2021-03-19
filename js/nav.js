3c35760d3f2a42f92fcf66c5e2369e7287566692
bradybassett <bradydbassett@gmail.com>
2021-03-18 22:35:47 -0500

    Created landing_page.js

--- js/nav.js
+++ js/nav.js
@@ -1,30 +1,25 @@
 $(function() {
-    function navToggle(){
-        var old_pos = $(document).scrollTop();
+    var old_pos = $(document).scrollTop();
 
-            $(window).scroll(function(){
-                var new_pos = $(document).scrollTop();
-                var toggled = false;
+    $(window).scroll(function(){
+        var new_pos = $(document).scrollTop();
+        var toggled = false;
 
-                if (new_pos > old_pos && toggled == false) {
-                    $('.navbar-toggle').css('transform', 'translateY(-90px)');
-                    toggled = true;
-                }
-                else {
-                    $('.navbar-toggle').css('transform', `translateY(0)`);
-                    toggled = false;
-                }
-                old_pos = new_pos;
+        if (new_pos > old_pos && toggled == false) {
+            $('.navbar-toggle').css('transform', 'translateY(-90px)');
+            toggled = true;
+        }
+        else {
+            $('.navbar-toggle').css('transform', `translateY(0)`);
+            toggled = false;
+        }
+        old_pos = new_pos;
 
-                if (toggled == false && new_pos != 0){
-                    $('.navbar-toggle').css('box-shadow', '2px 0px 15px');
-                }
-                else if (toggled == false && new_pos == 0){
-                    $('.navbar-toggle').css('box-shadow', '');
-                }
-        });
-    }
-
-    // Function Calls
-    navToggle();
+        if (toggled == false && new_pos != 0){
+            $('.navbar-toggle').css('box-shadow', '2px 0px 15px');
+        }
+        else if (toggled == false && new_pos == 0){
+            $('.navbar-toggle').css('box-shadow', '');
+        }
+    });
 });
