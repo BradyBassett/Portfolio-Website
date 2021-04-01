06c8216b4076224468c048c9c3ea9558387c9f6b
bradybassett <bradydbassett@gmail.com>
2021-04-01 17:02:54 -0500

    Made navbar transparent when at top of page

--- js/nav.js
+++ js/nav.js
@@ -11,12 +11,14 @@ $(function() {
         }
         else {
             $('.navbar-toggle').css('transform', `translateY(0)`);
+            $('nav').css('background-color', 'rgba(40, 40, 40, 0)')
             toggled = false;
         }
         old_pos = new_pos;
 
         if (toggled == false && new_pos != 0){
             $('.navbar-toggle').css('box-shadow', '2px 0px 15px');
+            $('nav').css('background-color', 'rgba(40, 40, 40, 1)')
         }
         else if (toggled == false && new_pos == 0){
             $('.navbar-toggle').css('box-shadow', '');
